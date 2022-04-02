a5635537d3fcc05e76515bd0825ff20858f9f830
Nikita Morunov <niknixad@gmail.com>
2022-04-02 20:30:06 +0400

    Добавьте файлы проекта.

---
+++ MegaQuestGameplay/MegaQuestGameplay.cpp
@@ -0,0 +1,12 @@
+﻿// MegaQuestGameplay.cpp: определяет точку входа для приложения.
+//
+
+#include "MegaQuestGameplay.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
